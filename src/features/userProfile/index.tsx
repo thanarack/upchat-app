efd7311dbcde7ea812eb692bc89f1df001973e87
Thanarack <thanarackc@gmail.com>
2022-06-07 22:48:23 +0700

    Added form hook.

--- src/features/userProfile/index.tsx
+++ src/features/userProfile/index.tsx
@@ -1,65 +1,151 @@
+/* eslint-disable jsx-a11y/alt-text */
+import { useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import isFileImage from '../../utils/isFileImage';
 import DashboardTemplate from '../dashboard/DashboardTemplate';
 
 const FeaturesProfileSetting = () => {
+  // Initialize necessary form function.
+  const { handleSubmit, register, watch, resetField } = useForm();
+
+  // Profile image handler
+  const [previewProfile, setPreviewProfile] = useState('/user-logo.png');
+  const inputFileRef: any = useRef();
+  const pickImageHandler = () => {
+    inputFileRef.current?.click();
+  };
+  const profileFiled = register('profile');
+  const displayImageProfile = (e: any) => {
+    const [file] = e.target.files;
+    if (file && isFileImage(file)) {
+      setPreviewProfile(URL.createObjectURL(file));
+    } else {
+      setPreviewProfile('/user-logo.png');
+      resetField('profile');
+    }
+  };
+  // End Profile image handler
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  console.log(watch());
+
   return (
     <DashboardTemplate isSearch={false} pageTitle="Profile - ข้อมูลผู้ใช้">
-      <div className="container flex justify-center mt-6">
-        <div className="md:max-w-2xl w-full">
-          <div className="px-4">
-            <div className="flex justify-center items-center flex-col">
-              <img
-                src="http://localhost:3000/user-logo.png"
-                className="w-24 h-24"
-              />
-              <div className="flex mt-2">
-                <button className="px-4 py-1 text-sm bg-neutral-200 rounded">
-                  เลือกรูป
+      <form id="profile-form" onSubmit={handleSubmit(onSubmit)}>
+        <div className="container flex justify-center mt-6">
+          <div className="md:max-w-2xl w-full">
+            <div className="px-4">
+              <div className="flex justify-center items-center flex-col">
+                <img
+                  title="profile"
+                  src={previewProfile}
+                  className="w-24 h-24 rounded-full"
+                />
+                <div className="flex mt-2">
+                  <input
+                    type="file"
+                    {...profileFiled}
+                    accept="image/*"
+                    ref={(instance) => {
+                      profileFiled.ref(instance);
+                      inputFileRef.current = instance;
+                    }}
+                    onChange={(e) => {
+                      profileFiled.onChange(e);
+                      displayImageProfile(e);
+                    }}
+                    className="hidden"
+                  />
+                  <button
+                    onClick={pickImageHandler}
+                    className="px-4 py-1 text-sm bg-neutral-200 rounded"
+                  >
+                    เลือกรูป
+                  </button>
+                </div>
+              </div>
+              <div className="form-group mt-6">
+                <label>ตำแหน่งปัจจุบัน</label>
+                <input
+                  type="text"
+                  className="form-input"
+                  readOnly
+                  {...register('position', { required: false })}
+                />
+              </div>
+              <div className="form-group">
+                <label>อีเมล</label>
+                <input
+                  type="email"
+                  className="form-input"
+                  readOnly
+                  {...register('email', { required: false })}
+                />
+              </div>
+              <div className="form-group">
+                <label>ชื่อ</label>
+                <input
+                  type="text"
+                  className="form-input"
+                  {...register('firstname', { required: true })}
+                />
+              </div>
+              <div className="form-group">
+                <label>นามสกุล</label>
+                <input
+                  type="text"
+                  className="form-input"
+                  {...register('lastname', { required: true })}
+                />
+              </div>
+              <div className="form-group">
+                <label>วันเกิด</label>
+                <input
+                  type="date"
+                  className="form-input"
+                  {...register('birthdate', { required: false })}
+                />
+              </div>
+              <div className="form-group">
+                <label>เพศ</label>
+                <div className="select-wrapper">
+                  <select
+                    className="form-input"
+                    {...register('gender', { required: false })}
+                  >
+                    <option>ชาย</option>
+                    <option>หญิง</option>
+                  </select>
+                </div>
+              </div>
+              <div className="form-group">
+                <label>เบอร์โทรติดต่อ</label>
+                <input
+                  type="tel"
+                  className="form-input"
+                  {...register('phone', { required: false })}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>ที่อยู่</label>
+                <textarea
+                  className="form-input"
+                  {...register('address', { required: false })}
+                />
+              </div>
+              <div className="text-right">
+                <button className="btn btn-green" form="profile-form">
+                  บันทึก
                 </button>
               </div>
             </div>
-            <div className="form-group mt-6">
-              <label>ตำแหน่งปัจจุบัน</label>
-              <input type="text" className="form-input" />
-            </div>
-            <div className="form-group">
-              <label>ชื่อ</label>
-              <input type="text" className="form-input" />
-            </div>
-            <div className="form-group">
-              <label>นามสกุล</label>
-              <input type="text" className="form-input" />
-            </div>
-            <div className="form-group">
-              <label>วันเกิด</label>
-              <input type="date" className="form-input" />
-            </div>
-            <div className="form-group">
-              <label>เพศ</label>
-              <div className="select-wrapper">
-                <select className="form-input">
-                  <option>ชาย</option>
-                  <option>หญิง</option>
-                </select>
-              </div>
-            </div>
-            <div className="form-group">
-              <label>เบอร์โทรติดต่อ</label>
-              <input type="tel" className="form-input" />
-            </div>
-            <div className="form-group">
-              <label>อีเมล</label>
-              <input type="email" className="form-input" />
-            </div>
-            <div className="form-group">
-              <label>ที่อยู่</label>
-              <textarea className="form-input" />
-            </div>
-            <div className="text-right">
-              <button className="btn btn-green">บันทึก</button>
-            </div>
           </div>
         </div>
-      </div>
+      </form>
     </DashboardTemplate>
   );
 };
